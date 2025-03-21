61b2431da98b648c4350ab3c92a8f274833a3766
guhkadoo <gustavokadoo@hotmail.com>
2025-03-21 11:50:42 -0300

    main.c file for tests of the wav library

---
+++ Projeto e Implementação de Sistemas/Algoritmos/wav/main.c
@@ -0,0 +1,11 @@
+#include "wav.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    wav wav_file;
+    if(read_wav("file_example3.wav", &wav_file) != -1)
+        printf("file_example read\n");
+
+    print_wav(&wav_file); 
+}
