e8a4eb8024e6bc083b83a45446b4ecc0c0cbf920
Gustavo Yujii Silva Kadooka <gustavo.kadooka@unesp.br>
2025-03-26 15:53:07 -0300

    added memory allocation to bmp.c

--- Projeto e Implementação de Sistemas/Algoritmos/bmp/bmp.c
+++ Projeto e Implementação de Sistemas/Algoritmos/bmp/bmp.c
@@ -23,22 +23,14 @@ int read_bmp(const char* filename, bmp* file_data)
         fclose(file);
         return -1;
     }
-
-    //size_t data_size = file_data->info_header.width * file_data->info_header.height * file_data->info_header.bits_per_pixel/8;
-
-    size_t sum = sizeof(file_data->header) + sizeof(file_data->info_header);
-    size_t data_size = file_data->header.file_size - sum; 
-    printf("data_offset: %d\nsizeof(bmp.header) + sizeof(info_header): %d\ndata_size: %d\n",
-            file_data->header.data_offset, sum, data_size);
-
-    printf("teste\nfile_size = %d\ninfo_header_size: %d\n", file_data->header.file_size, file_data->info_header.size); 
-
-    if(sum == file_data->header.data_offset)
+    if(file_data->info_header.bits_per_pixel > 8)
     {
         file_data->color_table = NULL;
     } else {
-        size_t color_table_size = 4 * file_data->info_header.bits_per_pixel;
+        size_t color_table_size = 4 * (1 << file_data->info_header.bits_per_pixel);
+        file_data->color_table = (uint8_t*)malloc(color_table_size);
         printf("color_table_size: %d\n", color_table_size);
+
         if(fread(file_data->color_table, color_table_size, 1, file) != 1)
         {
             printf("Error while reading bmp color_table\n");
@@ -46,6 +38,10 @@ int read_bmp(const char* filename, bmp* file_data)
             return -1;
         }
     }
+
+    size_t data_size = file_data->info_header.width * file_data->info_header.height * file_data->info_header.bits_per_pixel / 8; 
+    printf("teste\nfile_size = %d\ninfo_header_size: %d\ndata_size: %d\n", file_data->header.file_size, file_data->info_header.size, data_size); 
+    file_data->data = (uint8_t*)malloc(data_size);
 
     if(fread(file_data->data, data_size, 1, file) != 1)
     {
