2073355ea6f5519dac9daf790c40b935990aea87
guhkadoo <gustavokadoo@hotmail.com>
2025-03-30 16:22:49 -0300

    Created a child class of Huffman: HuffmanTXT

---
+++ Projeto e Implementação de Sistemas/Algoritmos/Huffman_Coding_Text_File/huffman_txt.cpp
@@ -0,0 +1,62 @@
+#include "huffman_txt.hpp"
+#include "../Huffman/huffman.hpp"
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+
+void HuffmanTXT::count_duplicates(uint8_t* data) 
+{
+    for(int i=0; data[i]!='\0'; i++)
+        duplicates[data[i]]++;
+}
+
+char* HuffmanTXT::encode(uint8_t* data)
+{
+    size_t size=0, i;
+    for(i=0; data[i]!='\0'; i++) {
+        size += strlen(dictionary[data[i]]);
+    }
+
+    char *code = (char *)calloc(size+1, sizeof(char));
+    for(i=0; data[i]!='\0'; i++) {
+        strcat(code, dictionary[data[i]]);
+    }
+
+    return code;
+}
+
+void HuffmanTXT::compress()
+{
+    uint8_t* data = read_text();
+    count_duplicates(data);
+    list.fill(duplicates);
+    tree.build(list);
+    size_t max_columns_of_dictionary = tree.height();
+    dictionary.build(max_columns_of_dictionary);
+    dictionary.fill(tree.root, "", max_columns_of_dictionary);
+    char* code = encode(data);
+    printf("%s\n", code);
+    internal_compress(code);
+}
+
+unsigned char* HuffmanTXT::read_text()
+{
+    FILE *file = fopen(filepath.c_str(), "rb");
+    unsigned char *str;
+    if(file != NULL) {
+        fseek(file, 0, SEEK_END);
+        size_t file_size = ftell(file);
+        rewind(file);
+
+        str = new unsigned char[file_size]();
+        size_t read_count = fread(str, sizeof(char), file_size, file);
+        str[read_count] = '\0';
+
+        fclose(file);
+    } else {
+        EXIT_WITH_ERROR("Error while executing read_text");
+    }
+
+    return str;
+}
---
+++ Projeto e Implementação de Sistemas/Algoritmos/Huffman_Coding_Text_File/huffman_txt.hpp
@@ -0,0 +1,18 @@
+#ifndef HUFFMAN_TXT_HPP 
+#define HUFFMAN_TXT_HPP
+
+#include "../Huffman/huffman.hpp"
+#include <cstdint>
+#include <cstdio>
+#include <string>
+
+class HuffmanTXT : public Huffman {
+public:
+    void count_duplicates(uint8_t* data); 
+    char* encode(uint8_t* data);
+    void compress();
+    unsigned char* read_text();
+    HuffmanTXT(std::string fp) : Huffman(fp) {};
+}; 
+
+#endif //HUFFMAN_TXT_HPP
