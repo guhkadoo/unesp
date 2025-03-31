0c9d1962ccbb26618cd209e19c64fc853d34eb30
guhkadoo <gustavokadoo@hotmail.com>
2025-03-31 09:20:23 -0300

    Created a child class of Huffman: HuffmanBMP

--- Projeto e Implementação de Sistemas/Algoritmos/Huffman_Coding_Image_File/huffman_bmp.hpp
+++ Projeto e Implementação de Sistemas/Algoritmos/Huffman_Coding_Image_File/huffman_bmp.hpp
@@ -3,7 +3,18 @@
 
 #include <cstdint>
 #include <cstdio>
+#include <string>
 #include "../bmp/bmp.hpp"
+#include "../Huffman/huffman.hpp"
+
+class HuffmanBMP : public Huffman {
+private:
+    bmp bmp_file;
+public:
+    void compress() override;
+    void decompress();
+    HuffmanBMP(std::string fp) : Huffman(fp) {};
+};
 
 
 #endif //HUFFMAN_BMP_HPP
