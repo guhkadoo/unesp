b40c2eed4f7776567d4291c9c8a156effec7af28
Gustavo Yujii Silva Kadooka <gustavo.kadooka@loop4.com.br>
2025-11-23 11:21:32 -0300

    Remocao da pasta de Projeto e Implementacao de Sistemas

--- Projeto e Implementação de Sistemas/GKompress/include/GUI.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef GUI_HPP
-#define GUI_HPP
-
-namespace GUI {
-    void init(int argc, char* argv[]);
-}
-
-#endif //GUI_HPP
--- Projeto e Implementação de Sistemas/GKompress/src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/GUI.hpp"
-
-int main(int argc, char* argv[]) {
-    GUI::init(argc, argv);
-
-    return 0;
-}
